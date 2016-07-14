0f1050e22885b36ff7277281171a71b7c0684244
Revant <revant.kapoor@gmail.com>
2016-07-13 23:03:28 -0700

    Add documentation

--- concrunner.go
+++ concrunner.go
@@ -1,4 +1,7 @@
-// Package concrunner is a
+// Package concrunner is a package for running a max number of functions concurrently.
+// It runs those functions keeping goroutines limited to the max number and then waits for them to be done
+// It also combines the results and errors of those functions.
+// You can use the package level methods to run a bunch of functions concurrently or create a new concrunner and enqueue functions to it. (Eg. in a for loop)
 package concrunner
 
 import (
@@ -80,6 +83,7 @@ var osErrorType = reflect.TypeOf((*error)(nil)).Elem()
 
 // Run a function with the given args.
 // This is useful when calling this from within a for loop when you want to use the looping variable
+// If the last return value of the function is of type error then it will combine that into the final MultiError
 func (p *concRunner) RunWithArgs(fn interface{}, args ...interface{}) {
 	v := reflect.ValueOf(fn)
 	if v.Kind() != reflect.Func {
